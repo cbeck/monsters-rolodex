a2fd01a184312cd88217dff16a38f69c106a58cb
Chris Beck <chris.beck@me.com>
2020-04-13 09:06:10 -0400

    Updates

---
+++ precache-manifest.1a1dc807bf12029797bbb18491e15646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b0c7587eb728938fc732b76c5f8e11",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a4fe30e4a22d10d8c3ce",
+    "url": "/monsters-rolodex/static/css/main.a2c384de.chunk.css"
+  },
+  {
+    "revision": "5aee1ac6ecf24a38689d",
+    "url": "/monsters-rolodex/static/js/2.340cdc49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.340cdc49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fe30e4a22d10d8c3ce",
+    "url": "/monsters-rolodex/static/js/main.0ee2a98a.chunk.js"
+  },
+  {
+    "revision": "94ac101b48457b533709",
+    "url": "/monsters-rolodex/static/js/runtime-main.23655a63.js"
+  }
+]);
